3f480827a90638609332ffc264974babf83edb22
VeturSkap <47110049+VeturSkap@users.noreply.github.com>
2019-05-24 07:41:36 +0300

    слайд 05

---
+++ lesson1/lists.py
@@ -0,0 +1,23 @@
+num=[3, 5, 7, 9, 10.5]
+print(num)
+num=[3, 5, 7, 9, 10.5]
+num.append("Python")
+print(len(num))
+num[0]
+print(num[0])
+num[5]
+print(num[5])
+num[1:4]
+print(num[1:4])
+del num[5]
+print(num)'''
+
+voc = {"city": "Москва", "temperature": "20"}
+'''print(voc["city"])
+voc["temperature"]=int(voc["temperature"])-5
+print(voc["temperature"])
+print(voc)
+print(voc["country"])'''
+voc.get("country", "Россия")
+voc["date"] = "27.05.2019"
+print(len(voc))
