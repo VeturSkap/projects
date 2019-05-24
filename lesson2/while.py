7573940fa178a15141e38454a5182d4a796e1940
VeturSkap <47110049+VeturSkap@users.noreply.github.com>
2019-05-24 18:37:15 +0300

    Create while.py

---
+++ lesson2/while.py
@@ -0,0 +1,6 @@
+def ask_user(user_say):
+    '''while True:
+        user_say = input('Как дела?: ')
+        if user_say == 'Хорошо':
+            break'''
+dictionary = [{'question':"Как дела", 'answer': "Хорошо!"},{'question': "Что делаешь?", 'answer': "Программирую"}]
