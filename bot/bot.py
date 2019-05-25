f32de4e83dae2e281fe9cc13bd250bd5f501eae3
Lyubov Snurnitsina <wintermoody@gmail.com>
2019-05-25 16:04:17 +0300

    ephim

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,16 @@
 from telegram.ext import Updater, CommandHandler
-
+import ephem
 def greet_user(bot, update):
     print('Вызван /start')
     update.message.reply_text('Я бот')
+
+def planets(bot, update):
+    print('Вызван /planet')
+    print(update.message.text.split( ))
+    name_planet=update.message.text.split( )
+    all_planet = {"Mars": ephem.Mars, "Saturn": ephem.Saturn, "Mercury": ephem.Mercury, "Venus": ephem.Venus, "Jupiter": ephem.Jupiter}
+    if name_planet[1] in all_planet:
+        print(ephem.constellation(all_planet[name_planet[1]](ephem.now())))
 
 def main():
     PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
@@ -12,6 +20,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planets))
 
     mybot.start_polling()
     mybot.idle()
