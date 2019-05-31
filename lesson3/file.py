3953be6ab039ad0896a4a67ad5783b24ff77023a
Lyubov Snurnitsina <wintermoody@gmail.com>
2019-05-31 10:50:50 +0300

    file

---
+++ lesson3/file.py
@@ -0,0 +1,10 @@
+with open('referat.txt', 'r', encoding='utf-8') as f:
+    text =f.read()
+    print(text)
+    text_len = len(text)
+    print(text_len)
+    for text != "\0":    
+        word_count = 0
+        if text == " ":
+            word_count += 1
+    print (word_count)
